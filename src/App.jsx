705e682c00b029338be2016160efee613141928a
lechinal <alin.lechintan@outlook.com>
2023-10-09 11:25:11 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,13 @@
 import React from 'react';
 
-const initialFriends = [
-  {
-    id: 118836,
-    name: 'Clark',
-    image: 'https://i.pravatar.cc/48?u=118836',
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: 'Sarah',
-    image: 'https://i.pravatar.cc/48?u=933372',
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: 'Anthony',
-    image: 'https://i.pravatar.cc/48?u=499476',
-    balance: 0,
-  },
-];
+import FriendsList from 'components/FriendsList/FriendsList';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      eat-n-split
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
     </div>
   );
 };
